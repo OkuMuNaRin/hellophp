91446de9f333dac8ac6e7aeee5c51aa0c9b88b95
wutao <wutao.88@qq.com>
2017-12-19 11:01:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
  * Time: 10:05
  */
 echo "<a href='/hellophp/?name=world'></a>";
-if($_SERVER['DOCUMENT_URI']=="/hellophp/") {
+if($_SERVER['DOCUMENT_URI']=="/") {
     require("class/hello.php");
     $name="world";
     if($_GET){
@@ -16,5 +16,5 @@ if($_SERVER['DOCUMENT_URI']=="/hellophp/") {
     $hello->sayHello($name);
 
 }else{
-    echo "<h1>无权访问</h1><a href='/hellophp/'>返回首页</a>";
+    echo "<h1>无权访问</h1><a href='/'>返回首页</a>";
 }
